84026f84af46c0847f8af255d165fbae4ab07680
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-10-26 16:02:17 -0400

    style SocialLinks

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -5,7 +5,10 @@ interface SocialLinkProps {
 
 export default function SocialLink({ href, children }: SocialLinkProps) {
   return (
-    <a href={href} className="block bg-grey-700">
+    <a
+      href={href}
+      className="block bg-grey-700 p-3 text-center rounded-lg text-sm font-bold leading-normal"
+    >
       {children}
     </a>
   );
