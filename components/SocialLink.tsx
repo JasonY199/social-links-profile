224316754be78935c7d977ab8d92620ee6f486c4
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-10-26 17:29:06 -0400

    add desktop social link hover states

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -7,7 +7,7 @@ export default function SocialLink({ href, children }: SocialLinkProps) {
   return (
     <a
       href={href}
-      className="block bg-grey-700 p-3 text-center rounded-lg text-sm font-bold leading-normal w-full"
+      className="transition-colors block bg-grey-700 p-3 text-center rounded-lg text-sm font-bold leading-normal w-full cursor-pointer hover:bg-brand-green hover:text-black"
     >
       {children}
     </a>
