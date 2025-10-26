316abd44b6da1e0f73947605d5b5c6d4792a62fd
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-10-26 14:45:39 -0400

    style author sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen p-6">
-      <article className="flex flex-col gap-6 rounded-xl bg-grey-800 p-6 text-brand-white max-w-[327px]">
+      <article className="flex flex-col gap-6 items-center rounded-xl bg-grey-800 p-6 text-brand-white max-w-[327px]">
         <Image
           src="/avatar-jessica.jpeg"
           alt="Jessica Randall"
@@ -11,15 +11,23 @@ export default function Home() {
           height={88}
           className="rounded-full height-[88px] w-[88px]"
         />
-        <h1>Jessica Randall</h1>
-        <p>London, United Kingdom</p>
-        <p>"Front-end developer and avid reader."</p>
-        <ul aria-label="Social Media links">
-          <li>link</li>
-          <li>link</li>
-          <li>link</li>
-          <li>link</li>
-          <li>link</li>
+        <div className="text-center">
+          <h1 className="text-2xl leading-normal font-semibold">
+            Jessica Randall
+          </h1>
+          <p className="text-sm font-bold leading-normal text-brand-green">
+            London, United Kingdom
+          </p>
+        </div>
+        <p className="text-sm leading-normal">
+          "Front-end developer and avid reader."
+        </p>
+        <ul aria-label="Social Media links" className="text-center">
+          <li>GitHub</li>
+          <li>Frontend Mentor</li>
+          <li>LinkedIn</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
         </ul>
       </article>
     </main>
